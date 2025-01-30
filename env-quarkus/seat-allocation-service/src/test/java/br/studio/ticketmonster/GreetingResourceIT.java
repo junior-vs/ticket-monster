25968a1007bf8a3e6a44a08e6a9742a2d84a394d
VALDIR JUNIOR <valdir.junior.dev@gmail.com>
2025-01-30 11:15:52 -0300

    criação de ambiente em microsservico

---
+++ env-quarkus/seat-allocation-service/src/test/java/br/studio/ticketmonster/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package br.studio.ticketmonster;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
