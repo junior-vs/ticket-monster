6e79f5d84eb081ec1dd93dbdf1c2dcccc64a3298
VALDIR JUNIOR <valdir.junior.dev@gmail.com>
2025-02-01 19:27:07 -0300

    Implement CRUD operations for events: add findById and list methods, and update EventResource with pagination support

--- env-quarkus/event-service/src/main/java/br/studio/ticketmonster/event/EventResource.java
+++ env-quarkus/event-service/src/main/java/br/studio/ticketmonster/event/EventResource.java
@@ -1,20 +1,32 @@
 package br.studio.ticketmonster.event;
 
 import java.net.URI;
+import java.util.List;
 
 import org.jboss.resteasy.reactive.RestResponse;
 
 import io.quarkus.hibernate.reactive.panache.common.WithTransaction;
 import io.smallrye.mutiny.Uni;
+
+import jakarta.enterprise.inject.Default;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DefaultValue;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.UriInfo;
 
+/**
+ * REST resource for managing crud events.
+ * <p>
+ * 
+ */
 @Path("/events")
+@WithTransaction
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class EventResource {
@@ -26,7 +38,6 @@ public class EventResource {
     }
 
     @POST
-    @WithTransaction
     public Uni<RestResponse<EventResponse>> createEvent(EventRequest event, @Context UriInfo uriInfo) {
         return eventService.createEvent(event)
                 .map(response -> {
@@ -38,4 +49,32 @@ public class EventResource {
                     return RestResponse.created(location);
                 });
     }
+
+    @GET
+    @Path("/{id}")
+    public Uni<RestResponse<EventResponse>> findById(Long id) {
+        return eventService.findById(id)
+                .map(response -> {
+                    if (response == null) {
+                        return RestResponse.notFound();
+                    } else {
+                        return RestResponse.ok(response);
+                    }
+                });
+    }
+
+    // lista com paginação
+    @GET
+    public Uni<RestResponse<List<EventResponse>>> list(@QueryParam("page") @DefaultValue("0") int pageIndex,
+            @QueryParam("size") @DefaultValue("20") int pageSize) {
+        return eventService.list(pageIndex, pageSize)
+                .map(response -> {
+                    if (response.isEmpty()) {
+                        return RestResponse.notFound();
+                    } else {
+                        return RestResponse.ok(response);
+                    }
+                });
+    }
+
 }
--- env-quarkus/event-service/src/main/java/br/studio/ticketmonster/event/EventService.java
+++ env-quarkus/event-service/src/main/java/br/studio/ticketmonster/event/EventService.java
@@ -1,10 +1,17 @@
 package br.studio.ticketmonster.event;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import io.quarkus.hibernate.reactive.panache.common.WithTransaction;
+import io.quarkus.panache.common.Page;
+import io.quarkus.panache.common.Sort;
+import io.quarkus.panache.common.Sort.Direction;
 import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
+@WithTransaction
 public class EventService {
 
     EventMapper eventMapper;
@@ -22,13 +29,27 @@ public class EventService {
      * @return
      */
 
-    @WithTransaction
     public Uni<EventResponse> createEvent(EventRequest event) {
         return Uni.createFrom()
                 .item(event) // Start with EventRequest
                 .map(eventMapper::toEntity) // Transform to Entity
                 .chain(eventRepository::persist) // Persist entity
                 .map(eventMapper::toResponse); // Transform to Response
+    }
+
+    public Uni<EventResponse> findById(Long id) {
+        return eventRepository.findById(id)
+                .map(eventMapper::toResponse);
+    }
+
+    public Uni<List<EventResponse>> list(int pageIndex, int pageSize) {
+
+        return eventRepository.findAll(Sort.by("id", Direction.Ascending))
+                .page(Page.of(pageIndex, pageSize))
+                .list()
+                .map(events -> events.stream()
+                        .map(eventMapper::toResponse)
+                        .collect(Collectors.toList()));
     }
 
 }
